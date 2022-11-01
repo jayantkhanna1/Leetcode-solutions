327a3af151ba6aa63cc28b894c2f0a6c79478bbd
Jayant Khanna <jayantkhanna3105@gmail.com>
2022-11-01 22:10:13 +0530

    Update 0003.cpp

--- src/0003-Longest-Substring-Without-Repeating-Characters/0003.cpp
+++ src/0003-Longest-Substring-Without-Repeating-Characters/0003.cpp
@@ -1,36 +1,30 @@
-#include <string>
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-static int x = []() {std::ios::sync_with_stdio(false); cin.tie(0); return 0; }();
-
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) 
-    {
-        int l = 0;
-        int r = 0;
-        int maxLength = 0;
-        int usedChar[256] = { 0 };
-        while (l < s.size())
+    int lengthOfLongestSubstring(string s) {
+        if(s.length()==0)return 0;   //if string of length zero comes simply return 0
+        unordered_map<char,int> m;   //create map to store frequency,(get to know all unique characters
+        int i=0,j=0,ans=INT_MIN; 
+        while(j<s.length())   
         {
-            if (r < s.size() && usedChar[s[r]] == 0)
+            m[s[j]]++;  //increase the frequency of the element as you traverse the string
+            if(m.size()==j-i+1)  // whem map size is equal to the window size means suppose window size is 3 and map size is also three that means in map all unique characters are their
             {
-                usedChar[s[r++]] += 1;
+                ans = max(ans,j-i+1);  //compare the length of the maximum window size
             }
-            else
+            else if(m.size()<j-i+1)   //if the map size is less than the window size means there is some duplicate present like window size = 3 and map size = 2 means there is a duplicates
             {
-                usedChar[s[l++]] -= 1;
+                while(m.size()<j-i+1)  //so till the duplicates are removed completely
+                {
+                    m[s[i]]--;   //remove the duplicates
+                    if(m[s[i]]==0)  //if the frequency becomes zero 
+                    {
+                        m.erase(s[i]);//delete it completely
+                    }
+                    i++;  //go for next element 
+                }
             }
-            maxLength = max(maxLength, r - l);
+             j++;  //go for the next element
         }
-        return maxLength;
+        return ans;
     }
 };
-int main()
-{
-    string s = "abcabcbb";
-    cout << Solution().lengthOfLongestSubstring(s) << endl;
-    return 0;
-}
