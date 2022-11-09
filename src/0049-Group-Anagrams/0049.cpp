d556dcc0571dbe5b1e4a70e2e4a6b80adc09b91e
Jayant Khanna <jayantkhanna3105@gmail.com>
2022-11-09 11:36:48 +0530

    Update 0049.cpp

--- src/0049-Group-Anagrams/0049.cpp
+++ src/0049-Group-Anagrams/0049.cpp
@@ -1,10 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
-using namespace std;
 
-static int x = [](){std::ios::sync_with_stdio(false);cin.tie(0);return 0;}();
 class Solution 
 {
 public:
@@ -23,16 +17,3 @@ public:
         return res;
     }
 };
-int main()
-{
-    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-    for (auto& i : Solution().groupAnagrams(strs))
-    {
-        for (auto& j : i)
-        {
-            cout << j << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
