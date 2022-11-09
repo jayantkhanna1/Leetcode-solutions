75d6bd7a035d29b001c3bc8bfc03d0f3a9261cc3
Jayant Khanna <jayantkhanna3105@gmail.com>
2022-11-09 08:59:59 +0530

    Update 0016.cpp

--- src/0016-3Sum-Closest/0016.cpp
+++ src/0016-3Sum-Closest/0016.cpp
@@ -1,32 +1,31 @@
-class Solution 
-{
+class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) 
-    {
-        vector<int> result;
+    int threeSumClosest(vector<int>& nums, int target) {
+        int min_sum = INT_MAX, ans;
         sort(nums.begin(), nums.end());
-        for (unsigned int i = 0; i < nums.size(); ++i)
-        {
-            int l = i + 1, r = nums.size() - 1;
-            int small = nums[l] + nums[l+1] + nums[i];
-            int big = nums[r] + nums[r-1] + nums[i];
-            if (small > target) result.push_back(small);
-            else if (big < target) result.push_back(big);
-            else
-            {
-                while (l < r)
-                {
-                    int tmp_sum = nums[l] + nums[r] + nums[i];
-                    result.push_back(tmp_sum);
-                    if (tmp_sum < target) ++l;
-                    else if (tmp_sum > target) --r;
-                    else return target;
+        for(int i=0; i<nums.size(); i++) {
+            int l = i + 1, r = nums.size() - 1; 
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum == target) {
+                    return sum;
+                }
+                else if(sum < target) {
+                    if(target - sum < min_sum) {
+                        min_sum = target - sum;
+                        ans = sum;
+                    }
+                    l++;
+                }
+                else if(sum > target) {
+                    if(sum - target < min_sum) {
+                        min_sum = sum - target;
+                        ans = sum;
+                    }
+                    r--;
                 }
             }
         }
-        sort(result.begin(), result.end(), [target](const int x1, const int x2){
-            return abs(x1 - target) < abs(x2 - target);
-        });
-        return result[0];
+        return ans;
     }
 };
