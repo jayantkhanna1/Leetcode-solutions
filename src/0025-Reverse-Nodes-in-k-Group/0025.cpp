e4180809b1a09a22af66902fc103ebf64391e2a2
Jayant Khanna <jayantkhanna3105@gmail.com>
2022-11-09 08:26:00 +0530

    Update 0025.cpp

--- src/0025-Reverse-Nodes-in-k-Group/0025.cpp
+++ src/0025-Reverse-Nodes-in-k-Group/0025.cpp
@@ -1,14 +1,3 @@
-#include <iostream>
-using namespace std;
-
-// Definition for singly-linked list.
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-static int x = []() {std::ios::sync_with_stdio(false); cin.tie(0); return 0; }();
 class Solution 
 {
 public:
@@ -42,7 +31,3 @@ public:
         return retNode;
     }
 };
-int main()
-{
-    return 0;
-}
